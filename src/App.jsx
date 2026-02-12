7c4a091fea7dafb4718360cf1ac208ad1b9b69ca
Seif Mansour <seifmansour365@gmail.com>
2026-02-12 21:55:49 +0200

    Nav bar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Header from "./layout/Header";
+import Navbar from "./layout/Navbar";
+
+
 
 export default function App () {
   return(
     <>
     <Header/>
+    <Navbar/>
     </>
   )
 }
---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar () {
+    return(
+        <nav>
+        <a href="home.html">Home | </a>
+        <a href="about.html">About Us | </a>
+        <a href="products.html">Products </a>
+        </nav>
+    )
+}
